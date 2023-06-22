128afc725d356f18c6f64e4fb192339ddf89ffed
esraorhan <esraorhan333@gmail.com>
2023-06-22 17:21:23 +0300

    Add project files.

---
+++ Core/DataAccess/IEntityRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess
+{
+    public interface IEntityRepository<T> where T :class,IEntity,new()
+    {
+        //T Referans tip olmalı bir class olmasına gerek yok bir interface de gönderilebilir T yerine
+        //kısıt olarak gönderilen nesne new lenebilir olması gerekir demektir. 
+        //Referans olan ,IEntity implemente edilen , newlenebilir herşeyi gönderebilirsin demektir.
+
+        T GET(Expression<Func<T,bool>> filter); //şarta göre bir nesne getirecek.
+        List<T> GETLIST(Expression<Func<T, bool>> filter = null); //C# Delege Kullanımı 
+        void ADD(T entity);
+        void UPDATE(T entity);
+        void DELETE(T entity);
+    }
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,13 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IProductDal : IEntityRepository<Product>
+    {
+        // artık IEntityRepository içinde yer alan Crud işlemlerinin hepsi var anlamına geliyor. 
+    }
+}
