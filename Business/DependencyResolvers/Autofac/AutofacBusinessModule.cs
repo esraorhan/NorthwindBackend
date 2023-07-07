00bd6cdfd7c235d10f840fc164f3863d455eb1d4
esraorhan <esraorhan333@gmail.com>
2023-07-07 11:06:29 +0300

    Autofac konfigrasyonu ve api tarafında konfigre edilmesi

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule :Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            //Constructer da IProductService şeklinde bişey isterse biz ona product manager veriyor olacağız.
+            builder.RegisterType<ProductManager>().As<IProductService>();           
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+        }
+    }
+}
