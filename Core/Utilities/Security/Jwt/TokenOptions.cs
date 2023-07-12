688f8da64f3807b4ab37bb112b446eb48ecc5b6c
esraorhan <esraorhan333@gmail.com>
2023-07-12 11:35:03 +0300

    Jwt Helper alt yapısının kurulması

---
+++ Core/Utilities/Security/Jwt/TokenOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Jwt
+{
+    public class TokenOptions
+    {
+        public string Audience { get; set; } //kullanıcı kitlesi
+        public string Issuer { get; set; } //imzalayan kisi
+        public int AccessTokenExpiration { get; set; } //token sona erme süresi
+        public string SecurityKey { get; set; }
+    }
+}
